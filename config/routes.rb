5ac739e40986fbd274a366362e9faff43bb0839e
Miguel Ballén <migaballengal@unal.edu.co>
2017-10-23 22:27:48 -0500

    Chore:Limiting routing matches in Routes.rb for sites

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :sites
-
+ resources :sites, , only: [:index, :show, :create, :update, :destroy]
  resources :events, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'events#index'
